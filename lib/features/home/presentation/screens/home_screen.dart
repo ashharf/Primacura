bef1a3f107dece424477c5e9c38238c0c2b7c6c0
Mubashshir Firdausi <mubashshir@haabtek.com>
2024-12-21 13:38:17 +0530

    fix: increase height of date range picker container in home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -124,7 +124,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Container(
               padding: AppConstants.defaultPading,
-              height: 250,
+              height: 300,
               child: SfDateRangePicker(
                 headerStyle: DateRangePickerHeaderStyle(
                   backgroundColor: context.read<ThemeProvider>().currentThemeBrightness == Brightness.light
